515e24545a3870580fc9c7f97756740a75290777
evhen <evhen-horetskyy>
2021-07-11 02:47:27 +0300

    Lab4

---
+++ Lab4/logs_reader.py
@@ -0,0 +1,23 @@
+import sys
+
+from sqlalchemy import create_engine
+
+ 
+
+engine = create_engine('sqlite:///access_logs.db', echo = True)
+
+connection = engine.connect()
+
+ip_address = sys.argv[1]
+
+ 
+
+text = "SELECT * FROM access_logs WHERE ip_address=:ip"
+
+result = connection.execute(text, ip = ip_address).fetchall()
+
+connection.close()
+
+for data in result:
+
+ print(data)
