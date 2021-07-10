16c31eb49ecce5fb20b0960f9843546846dc957f
LeonidRiabov <86837335+LeonidRiabov@users.noreply.github.com>
2021-07-10 22:26:16 +0300

    Delete Lab3-2 directory

--- Lab3-2/task1.py
+++
@@ -1,6 +0,0 @@
-numb = int(input("Enter an integer: "))
-count = 0
-for i in range(numb - 1, 1, -1):
-    if (numb % i == 0):
-        count += 1
-print("Number of divisors:", count)
--- Lab3-2/task2.py
+++
@@ -1,5 +0,0 @@
-import numpy
-print("Input numbers:")
-arr = input()
-l = list(map(float,arr.split(' ')))
-print(numpy.mean(l))
