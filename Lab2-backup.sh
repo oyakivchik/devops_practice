50eea7239d651617cf7710c0356951f819dbf496
Denis Savchuk <kbob88@gmail.com>
2021-07-13 22:51:50 +0300

    third lab commit of the task 1

--- Lab2-backup.sh
+++ Lab2-backup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#echo "Enter the path of file for backup"
 
-#read path
 tar -cvzf ~/Documents/backup/backup-`date +%s`.tar.gz $1
 
---
+++ Lab3-2/task1.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+def number_of_divers(number):
+    counter = 0
+    for i in range(1, number + 1):
+        if number % i == 0:
+            counter += 1
+    return print(counter)
+
+task1, number = argv
+number_of_divers(int(number))
