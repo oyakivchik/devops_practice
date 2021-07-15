7fdaf71b93e10badb6f86605f935ee7e160c2443
Ivan Tsaryk <48809714+Aiveng0@users.noreply.github.com>
2021-07-15 21:12:11 +0300

    Add files via upload

---
+++ Lab 2/lab2-rename.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "INPUT DIRECTORY PATH: "
+read path
+echo "YOUR PATH: $path"
+num=0
+cd $path
+for file in *
+do 
+   (( num ++ ))	
+	if [ */$num-"$file" ];
+	then
+	echo "FILE $file RENAMED"
+	else
+	mv "$file" $num-"$file"
+fi
+done 
+ls 
+cd ~
