a70f925ec4be8ed34fc39030ae9b9536870df697
Andre Filipe Georgievich <voroncka.andrey@gamil.com>
2021-07-16 22:06:01 +0300

    Lb4 fix logs_parser.py insert

--- Lb4/logs_parser.py
+++ Lb4/logs_parser.py
@@ -14,7 +14,7 @@ access_logs = Table(
     Column('id',Integer,primary_key=True),
     Column('hostname',String),
     Column('ip_address',String),
-    Column('date_time',DateTime, default=datetime.now()),
+    Column('date_time',DateTime),
     Column('message',String),
 )
 
@@ -27,28 +27,25 @@ conn = engine.connect()
 #opening file "access.log" for "read"
 f = open(sys.argv[1],'r') #'access.log'
 
+ins= []
+print('Loading...')
 #loop through all lines
 for line in f:
     validation = re.compile(r'^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+(sshd)\S+:\s+(.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*)$').search(line)
     if validation:
-        print(validation)
-        print('\n0: ',validation.group(0))
-        print('\n1: ',validation.group(1))
-        print('\n2: ',validation.group(2))
-        print('\n3: ',validation.group(3))
-        print('\n4: ',validation.group(4))
-        print('\n5: ',validation.group(5))
-        print('\n6: ',validation.group(6))
-        ins = access_logs.insert().values(
-            hostname= validation.group(3),
-            ip_address= validation.group(6),
-            message= validation.group(5))
+        ins.append({
+            "hostname" : validation.group(3),
+            "ip_address" : validation.group(6),
+            "date_time" : datetime.now(),
+            "message" : validation.group(5)
+            })
+print('Done')
 
 #closing the file "access.log"
 f.close()
 
 #inserting data to database
-result = conn.execute(ins)
+result = conn.execute(access_logs.insert(),ins)
 
 #closing the connection
 conn.close()
