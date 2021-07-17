8a0eb21d1be1327ef75f003acb01c1a4df352ca8
Andriy Meleniuk <amelentium@gmail.com>
2021-07-17 17:41:02 +0300

    Create Lab3-1

---
+++ Lab3-1/task2.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+from sys import argv
+from re import match
+
+
+def sumOfSquares(nums):
+    _numbers = []
+    _sum = 0
+
+    for num in nums:
+        if match('^(\d+|(\d*[.,]?\d+))$', num):
+            num = num.replace(',', '.')
+            _numbers.append(num)
+            _sum += (pow(float(num), 2))
+
+    if len(_numbers):
+        print('The sum of the squares of the ' + ", ".join(_numbers) + ' is ' + str(_sum))
+    else:
+        print('No numeric parameters received')
+
+
+args = argv[1:] if argv[1:] else 0
+
+print('No parameters received') if args == 0 else sumOfSquares(args)
