c4dffcd06b4bd21c515815c2047144326ffe7a90
IntolerantOne <87035879+IntolerantOne@users.noreply.github.com>
2021-07-18 16:47:15 +0300

    Update logs_reader.py

--- Lab4/logs_reader.py
+++ Lab4/logs_reader.py
@@ -5,7 +5,7 @@ engine = create_engine('sqlite:///access_logs.db', echo = False)
 connection = engine.connect()
 
 user_ip = sys.argv[1] 
-que = "SELECT * FROM access_logs WHERE ip_address=:ip" 
+que = "SELECT * FROM access_logs WHERE user_ip=:ip" 
 info = connection.execute(que, ip = user_ip).fetchall()
 connection.close()
 
