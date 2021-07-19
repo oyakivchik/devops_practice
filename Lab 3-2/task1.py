225578edc468038aace6644a0987135acf3adc45
Odi-cs <fry.pichenka@gmail.com>
2021-07-19 13:49:04 +0300

    lab 3 via system args

--- Lab 3-2/task1.py
+++ Lab 3-2/task1.py
@@ -1,5 +1,6 @@
-print("input number: ")
-num = int(input())
+import sys
+
+num = int(sys.argv[1])
 print("dividers:")
 for i in range(2, num-1):
     if num % i == 0:
--- Lab 3-2/task2.py
+++ Lab 3-2/task2.py
@@ -1,5 +1,8 @@
-print("input numbers: ")
-x1, x2, x3 = [int(i) for i in input().split()]
+import sys
+
+x1=int(sys.argv[1])
+x2=int(sys.argv[2])
+x3=int(sys.argv[3])
 y = (x1+x2+x3)/3
 y = round(y,3)
 print(f'average:{y}')
