f2642e11914e2f04eba9afa06f786e77e0a020c8
Andre Filipe Georgievich <voroncka.andrey@gamil.com>
2021-07-19 18:48:29 +0300

    Lb5 fix yaml files #7

--- Lb5/trigger_workflow_dispatch_event.py
+++ Lb5/trigger_workflow_dispatch_event.py
@@ -1,14 +1,13 @@
-import requests
+# import requests
 
-response = requests.post(
-    'https://github.com/repos/oyakivchik/devops_practice/actions/workflows/andre-filipe-georgievich-344sk-lb5/dispatches',
-    params={'q': 'requests+language:python'},
-    headers={'Accept': 'application/vnd.github.v3.text-match+json'},
-    data={'ref':'andre-filipe-georgievich-344sk'}
-)
-print(response.json())
-# View the new `text-matches` array which provides information
-# about your search term within the results
+# response = requests.post(
+#     'https://github.com/repos/oyakivchik/devops_practice/actions/workflows/andre-filipe-georgievich-344sk-lb5.yml/dispatches',
+#     headers={'Accept': 'application/vnd.github.v3.text-match+json'},
+#     data={'ref':'andre-filipe-georgievich-344sk'}
+# )
+# print(response.json())
+# # View the new `text-matches` array which provides information
+# # about your search term within the results
 # json_response = response.json()
 # print(json_response)
 # repository = json_response['items'][0]
