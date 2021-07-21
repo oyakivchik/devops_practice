16f66152d3a57b611eb68071307892c7b45d9b29
Victoria156 <87039682+Victoria156@users.noreply.github.com>
2021-07-21 23:03:09 +0300

    Create ip_email.py

---
+++ lab5/ip_email.py
@@ -0,0 +1,47 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from sqlalchemy import *
+from sqlalchemy.sql import select
+
+engine = create_engine('sqlite:///access_logs.db', echo=False)
+meta = MetaData()
+result_list = ''
+
+access_logs = Table(
+    'access_logs', meta,
+    Column('id',Integer,primary_key=True),
+    Column('hostname',String),
+    Column('ip_address',String),
+    Column('date_time',DateTime),
+    Column('message',String),
+)
+
+conn = engine.connect()
+query = select(access_logs.c.ip_address).distinct()
+result = conn.execute(query)
+
+for row in result:
+    result_list += str(row.ip_address)+'\n'
+    print(row.ip_address)
+
+conn.close()
+
+sender_email = "vikashyshkina121@gmail.com"
+receiver_email = "olexiy.jakivtchik@gmail.com"
+password = "1234554321Vika!"
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "123123"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+text = result_list
+mess = MIMEText(text, "plain")
+message.attach(mess)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465 , context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+print('Completed')
