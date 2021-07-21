aa68eb25735514e86c655c10ae8a10f393a33969
horodynskyi <horodynskyi@users.noreply.github.com>
2021-07-21 19:57:57 +0300

    Lab4

---
+++ Lab4/logs_reader.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+from sqlalchemy import Column, DateTime, Integer, MetaData, String, Table
+from sqlalchemy import create_engine
+
+access_logs = Table(
+    'access_logs', MetaData(),
+    Column('id', Integer, primary_key=True),
+    Column('hostname', String),
+    Column('ip_address', String),
+    Column('date_time', DateTime),
+    Column('message', String),
+)
+
+
+def printLogs():
+    engine = create_engine('sqlite:///access_logs.db', echo=False)
+    connection = engine.connect()
+    logs = connection.execute(access_logs.select())
+
+    for log in logs:
+        print(f'{log.date_time} {log.hostname} {log.message}')
+
+
+printLogs()
