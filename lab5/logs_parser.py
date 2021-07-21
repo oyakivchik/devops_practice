2a5004ed7cca02287f4a33180e2d8add17e2f60b
Victoria156 <87039682+Victoria156@users.noreply.github.com>
2021-07-21 22:04:26 +0300

    Create logs_parser.py

---
+++ lab5/logs_parser.py
@@ -0,0 +1,38 @@
+from sqlalchemy import *
+import re
+import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", help="Path:", required=True)
+args = parser.parse_args()
+path = args.path
+
+engine = create_engine('sqlite:///access_logs.db')
+meta = MetaData()
+
+access_logs = Table(
+   'access_logs', meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('hostname', String),
+   Column('ip_address', String), 
+   Column('date_time', DateTime),
+   Column('message', String),
+)
+
+meta.create_all(engine)
+
+conn = engine.connect()
+logs_entries = []
+file = open(path, 'r')
+ 
+for entry in file:
+    line = re.compile(r'^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+(sshd)\S+:\s+(.?(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).)$').search(entry)
+    if line:
+        datetime_str = line.group(1) + " " + str(datetime.datetime.now().year)
+        datetime_obj = datetime.datetime.strptime(datetime_str, '%b %d %H:%M:%S %Y')
+        logs_entries.append({"hostname": line.group(3), "ip_address": line.group(6), "date_time": datetime_obj, "message": line.group(5)})
+
+result = conn.execute(access_logs.insert(None), logs_entries)
+file.close()
+conn.close()
