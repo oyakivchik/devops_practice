e5b8d4be4e553c6dcead19f274fefb2e1405439e
murranik <60602487+murranik@users.noreply.github.com>
2021-07-22 21:13:21 +0300

    Add files via upload

---
+++ Lab4/logs_reader.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+from sqlalchemy import create_engine
+
+
+def printLogs():
+    engine = create_engine('sqlite:///access_logs.db', echo=False)
+    connection = engine.connect()
+    logs = connection.execute(access_logs.select())
+
+    for log in logs:
+        print(f'{log.date_time} {log.hostname} {log.message}')
+
+
+printLogs()
