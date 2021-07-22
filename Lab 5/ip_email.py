e61ae49abb2b200b44865a9f2ef089685cb906f0
Ilarion2 <87083365+Ilarion2@users.noreply.github.com>
2021-07-22 12:38:35 +0300

    Add files via upload

--- Lab 5/ip_email.py
+++ Lab 5/ip_email.py
@@ -1,30 +1,57 @@
-import sys
-from email.mime.multipart import MIMEMultipart
+# -*- coding: utf-8 -*-
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
+from sqlalchemy.sql import select
+import smtplib, ssl
 from email.mime.text import MIMEText
-from smtplib import SMTP_SSL
-from sqlalchemy import create_engine
-from ssl import create_default_context
+from email.mime.multipart import MIMEMultipart
 
-filename = sys.argv[1]
-email = sys.argv[2]
-password = sys.argv[3]
-receiver = sys.argv[4]
-subject = 'ilarion-gordeychik-341sk Lab5'
+engine = create_engine('sqlite:///access.db', echo=False)
+meta = MetaData()
 
-engine = create_engine('sqlite:///{}'.format(filename), echo = False)
+access_logs = Table(
+    'access_logs', meta,
+    Column('id',Integer,primary_key=True),
+    Column('hostname',String),
+    Column('ip_address',String),
+    Column('date_time',DateTime),
+    Column('message',String),
+)
+
+print('Loading...')
 conn = engine.connect()
-ip = conn.execute("SELECT DISTINCT ip_address from access_logs")
-body = 'Unique ips: \n'+', '.join(map(lambda row: row['ip_address'],ip))
+sel = select(access_logs.c.ip_address).distinct()
+result = conn.execute(sel)
 
-message = MIMEMultipart("alternative")
-message["Subject"] = subject
-message["From"] = email
-message["To"] = receiver
-message.attach(MIMEText(body, "plain"))
-
-context = create_default_context()
-with SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-	server.login(email, password)
-	server.sendmail(email, receiver, message.as_string())
+res_list = ''
+print('\n')
+for row in result:
+    res_list += '<li><i><b>'+row.ip_address+'</b></i></li>\n'
+    print(row.ip_address)
 
 conn.close()
+print('Done')
+
+sender_email = "loklav228@gmail.com"
+receiver_email = "olexiy.jakivtchik@gmail.com"
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "Ilarion-Gordeychik-341sk Lab5"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+# Create the plain-text of your message
+text = """\
+Test"""
+
+part1 = MIMEText(text, "plain")
+message.attach(part1)
+
+print('Sending email...')
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465 , context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
+print('Done')
--- Lab 5/logs_parser.py
+++ Lab 5/logs_parser.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
-import datetime
+from datetime import datetime
 import re
 import sys
 
+#creating_engine
 engine = create_engine('sqlite:///access.db', echo=False)
 meta = MetaData()
 
+#defining table "access_logs"
 access_logs = Table(
     'access_logs', meta,
     Column('id',Integer,primary_key=True),
@@ -15,25 +18,35 @@ access_logs = Table(
     Column('message',String),
 )
 
+#create table
 meta.create_all(engine)
-connection = engine.connectionect()
-file = open(sys.argv[1],'r') 
 
-for lines in file:
-    line = re.findall('^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+(sshd)\S+:\s+(.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*)$',line)
-    if line:
-        print(line)
-        print('\n0 Date:',line[0][0])
-        print('\n1 Month:',line[0][1])
-        print('\n2 Something:',line[0][2])
-        print('\n3 Hostname:',line[0][3])
-        print('\n4 Message:',line[0][4])
-        print('\n5 Ip address:',line[0][5])
-        ins = access_logs.insert().values(
-            hostname= line[0][3],
-            ip_address= line[0][5],
-            message= line[0][4])
+#connect to database
+conn = engine.connect()
 
-file.close()
-result = connection.execute(ins)
-connection.close()
+#opening file "access.log" for "read"
+f = open(sys.argv[1],'r') #'access.log'
+
+ins= []
+
+print('Loading...')
+#loop through all lines
+for line in f:
+    validation = re.compile(r'^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+(sshd)\S+:\s+(.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*)$').search(line)
+    if validation:
+        ins.append({
+            "hostname" : validation.group(3),
+            "ip_address" : validation.group(6),
+            "date_time" : datetime.now(),
+            "message" : validation.group(5)
+            })
+
+#closing the file "access.log"
+f.close()
+
+#inserting data to database
+result = conn.execute(access_logs.insert(), ins)
+
+#closing the connection
+conn.close()
+print('Done')
