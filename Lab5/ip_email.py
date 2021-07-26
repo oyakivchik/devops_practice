1b54c8572c52734ff0413b7c5d815b7d0b96abb8
Koder-Kat <78554996+Koder-Kat@users.noreply.github.com>
2021-07-26 18:27:23 +0300

    Add files via upload

--- Lab5/ip_email.py
+++ Lab5/ip_email.py
@@ -1,1 +1,28 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from smtplib import SMTP_SSL
+from sqlalchemy import create_engine
+from ssl import create_default_context
+import sys
 
+login = sys.argv[1]
+password = sys.argv[2]
+receiver = sys.argv[3]
+
+engine = create_engine('sqlite:///access_logs.db', echo = False)
+connection = engine.connect()
+ips = connection.execute("SELECT DISTINCT ip_address from access_logs")
+
+subject = 'Буковська-Анна-32 Lab5'
+body = 'Ip list:\n' + ', '.join(map(lambda row: row['ip_address'],ips))
+
+message = MIMEMultipart("alternative")
+message["Subject"] = subject
+message["From"] = login
+message["To"] = receiver
+message.attach(MIMEText(body, "plain"))
+
+context = create_default_context()
+with SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(login, password)
+    server.sendmail(login, receiver, message.as_string())
